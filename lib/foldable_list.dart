a7d4546e747f8e6c619691eada04a12af7ed3b44
Kouki Badr <badreddinekouki1998@gmail.com>
2021-07-03 08:33:51 +0100

    migrate project to null safety

--- lib/foldable_list.dart
+++ lib/foldable_list.dart
@@ -1,7 +1,16 @@
-library foldable_list;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class FoldableList extends StatefulWidget {
+  
+  @override
+  _FoldableListState createState() => _FoldableListState();
+}
+
+class _FoldableListState extends State<FoldableList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
 }
