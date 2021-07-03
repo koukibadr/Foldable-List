5dbc04548fd133d62302b3f65a0a6685e79e7478
Kouki Badr <badreddinekouki1998@gmail.com>
2021-07-03 09:08:35 +0100

    create foldable list widget  with two required parameters

--- lib/foldable_list.dart
+++ lib/foldable_list.dart
@@ -1,16 +1,55 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class FoldableList extends StatefulWidget {
-  
+  final List<Widget> foldableItems;
+  final List<Widget> items;
+
+  FoldableList({required this.foldableItems, required this.items});
+
   @override
   _FoldableListState createState() => _FoldableListState();
 }
 
 class _FoldableListState extends State<FoldableList> {
+  List<Widget> displayedList = [];
+  int currentSelectedItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    displayedList.addAll(this.widget.items);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ListView.builder(
+      itemCount: this.displayedList.length,
+      itemBuilder: (context, index) {
+        return InkWell(
+          onTap: () {
+            _updateSelectedItem(index);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: this.displayedList[index],
+          ),
+        );
+      },
     );
+  }
+
+  _updateSelectedItem(position) {
+    displayedList.clear();
+    displayedList.addAll(this.widget.items);
+    setState(() {
+        displayedList[position] = this.widget.foldableItems[position];
+    });
+    // displayedList[currentSelectedItem] = this.widget.items[currentSelectedItem];
+    // setState(() {
+    //   displayedList[position] = this.widget.foldableItems[position];
+    //   currentSelectedItem = position;
+    // });
   }
 }
