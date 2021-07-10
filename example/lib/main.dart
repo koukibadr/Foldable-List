d20a04ec68683f2561e1fa0dce0d1932a3b2727c
Kouki Badr <badreddinekouki1998@gmail.com>
2021-07-10 10:32:36 +0100

    udpate package example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,136 +20,111 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Widget> simpleWidgetList;
+  late List<Widget> expandedWidgetList;
+
+  @override
+  void initState() { 
+    super.initState();
+    initList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
         child: FoldableList(
-          animationType: ANIMATION_TYPE.SCALE,
-          foldableItems: [
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-          ),
-        ], items: [
-          Container(
-            width: 200,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 200,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 150,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 150,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 150,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 150,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 150,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-          Container(
-            width: 150,
-            height: 90,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-          ),
-        ]),
+          animationType: ANIMATION_TYPE.SIZE_TRANSITION,
+          foldableItems: this.expandedWidgetList, 
+          items: this.simpleWidgetList
+        ),
+      ),
+    );
+  }
+
+  initList(){
+    this.simpleWidgetList = [];
+    this.expandedWidgetList = [];
+    for (var i = 0; i < 9; i++) {
+      this.simpleWidgetList.add(renderSimpleWidget());
+      this.expandedWidgetList.add(renderExpandedWidget());
+    }
+  }
+
+  Widget renderSimpleWidget(){
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.account_circle_outlined),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("John Doe",style: TextStyle(
+                  fontWeight: FontWeight.bold
+                )),
+                Text("Web developer and engineer"),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget renderExpandedWidget(){
+    return Container(
+      height: 140,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.account_circle_outlined),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("John Doe",style: TextStyle(
+                  fontWeight: FontWeight.bold
+                )),
+                Text("Web developer and engineer"),
+                Text("Birthday : 22/10/1996"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(onPressed: (){}, child: Text("View Linkedin Profile"))
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
